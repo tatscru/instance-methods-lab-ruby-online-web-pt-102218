d16328f53ef770d246642101cd4c0d4a51744722
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-09 03:54:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog 
+  # class name is always capitalized 
+  def bark 
+    # this is an instance because it is within a class
+    puts "Woof!"
+    
+  def sit
+    puts "The Dog is sitting"
+  end 
+  
+  end 
+end 
